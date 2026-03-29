337f3d8b00a9befac589f29a1ce2ff02da10c1cd
Jonas Lindau <jonas@jonika.nu>
2026-03-29 15:01:33 +0200

    Added icon and color handling

--- extension.js
+++ extension.js
@@ -17,6 +17,7 @@
  */
 
 import GObject from 'gi://GObject';
+import Gio from 'gi://Gio';
 import St from 'gi://St';
 
 import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
@@ -30,27 +31,31 @@ import * as Risk from './risk.js';
 
 const Indicator = GObject.registerClass(
     class Indicator extends PanelMenu.Button {
-        _init() {
+        _init(extension) {
             super._init(0.0, _('My Shiny Indicator'));
 
             // local variables
-            this._location = null;
+            //this._location = null;
+
+            // Create tray icon and change color to white
+            this._icon = new St.Icon({
+                gicon: Gio.icon_new_for_string(`${extension.path}/resources/fire-symbolic.svg`),
+                style_class: 'system-status-icon',
+            })
+            this._icon.set_style("color: #fff !important;");
 
             // Add tray icon
-            this.add_child(new St.Icon({
-                icon_name: 'face-smile-symbolic',
-                style_class: 'system-status-icon',
-            }));
+            this.add_child(this._icon);
 
             // Create menu item and disable click for now
-            this._riskItem = new PopupMenu.PopupMenuItem(_('Hämtar position...'));
-            this._riskItem.setSensitive(false);
-            this.menu.addMenuItem(this._riskItem);
+            // this._riskItem = new PopupMenu.PopupMenuItem(_('Hämtar position...'));
+            // this._riskItem.setSensitive(false);
+            // this.menu.addMenuItem(this._riskItem);
 
             // Try to fetch position
-            Geo.getLocation().then(loc => {
+            Geo.getLocation().then(locJson => {
                 // If no position is found, set label
-                if (!loc) {
+                if (!locJson) {
                     // Log unknown position
                     log("Position: unknown!");
 
@@ -60,32 +65,70 @@ const Indicator = GObject.registerClass(
                 }
 
                 // Log position for debug
-                log(`Position: ${loc.lat}, ${loc.lon} (${loc.city}, ${loc.country})`);
+                log(`Position: ${locJson.lat}, ${locJson.lon} (${locJson.city}, ${locJson.country})`);
 
                 // Store position in local variable
-                this._location = loc;
+                //this._location = loc;
+
+                Risk.getRisk(locJson.lat, locJson.lon, 'sv').then(riskJson => {
+                    // log json for debug
+                    log('----------------------------------------------');
+                    log(riskJson);
+
+                    // Set icon color based in risk
+                    this._setIconcolor(riskJson.risk);
+                });
 
                 // Set label with city
-                this._riskItem.label.text = `Visa brandrisk i ${loc.city}`;
+                //this._riskItem.label.text = `Visa brandrisk i ${loc.city}`;
 
                 // Enable click since we have a position
-                this._riskItem.setSensitive(true);
-                this._riskItem.connect('activate', () => {
-                    Risk.getRisk(loc.lat, loc.lon, 'sv').then(risk => {
-                        log('----------------------------------------------');
-                        log(risk);
+                //this._riskItem.setSensitive(true);
+                // this._riskItem.connect('activate', () => {
+                //     Risk.getRisk(loc.lat, loc.lon, 'sv').then(risk => {
+                //         // log json for debug
+                //         log('----------------------------------------------');
+                //         log(risk);
 
-                        Main.notify(risk.riskMessage);
-                    });
-                    //Main.notify(`Din ungefärliga position är ${loc.lat}, ${loc.lon} - ${loc.city}, ${loc.country}`);
-                });
+                //         // Set icon color based in risk
+                //         this._setIconcolor(risk.riskIndex);
+
+                //         // Show risk message
+                //         Main.notify(risk.riskMessage);
+                //     });
+                // });
             });
+        }
+
+        _setIconcolor(riskIndex) {
+            // default color, gray
+            let color = "#7a7a7a";
+
+            // Get color based on riskIndex
+            switch (riskIndex) {
+                case 1: color = "#2ecc71"; break;
+                case 2: color = "#f1c40f"; break;
+                case 3: color = "#e67e22"; break;
+                case 4: color = "#e74c3c"; break;
+                case 5: color = "#c0392b"; break;
+                case 6: color = "#8e0000"; break;
+            }
+
+            // build the style
+            let style = `color: ${color} !important;`;
+
+            // log for debug
+            log('-------------------------------------');
+            log(style);
+
+            // set style
+            this._icon.set_style(`color: ${color} !important;`);
         }
     });
 
 export default class IndicatorExampleExtension extends Extension {
     enable() {
-        this._indicator = new Indicator();
+        this._indicator = new Indicator(this);
         Main.panel.addToStatusArea(this.uuid, this._indicator);
     }
 
